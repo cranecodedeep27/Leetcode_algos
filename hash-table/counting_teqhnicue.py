3aaac238e8d4e00598a9502559d67efe51c61bd4
Daniil Zhuravskii <zhuravskiidaniil07@gmail.com>
2026-09-23 19:35:58 +0300

    Техника подсчёта

---
+++ hash-table/counting_teqhnicue.py
@@ -0,0 +1,20 @@
+def is_palindrome_permutations(s: str) -> bool:
+    count = {} # ключ - символ, значение - частота
+    
+    for char in s:
+        # Подсчитываем частоту
+        count[char] = count.get(char, 0) + 1
+        
+    odd_count = 0
+    # проверяем условие
+    for freq in count.values():
+        if freq % 2 == 1:
+            odd_count += 1
+            
+    return odd_count <= 1
+# Два примера
+print(is_palindrome_permutations("abddab"))
+print(is_palindrome_permutations("absqqs"))
+
+# Время: O(n)
+# Память: O(k)
